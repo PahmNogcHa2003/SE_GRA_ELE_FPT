3f7146527136c007011374b4f39239e0d46d477d
TrungHa-IT <hatrung03022003@gmail.com>
2025-08-07 00:05:21 +0700

    Initial base project

---
+++ Solution1/SE_GRA_ELE_FPT-DBAccess/ApplicationDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SE_GRA_ELE_FPT_DBAccess;
+
+namespace SE_GRA_ELE_FPT_DBAccess
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // Set the base path to the directory containing the appsettings.json file
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../SE_GRA_ELE_FPT-API");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
