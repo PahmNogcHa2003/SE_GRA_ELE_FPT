2557b9002b47fdbf109e739b12f4be3b50099fcd
TrungHa-IT <hatrung03022003@gmail.com>
2025-10-02 10:55:49 +0700

    add auto mapper dtos and domain

--- backend/src/backend/API/Program.cs
+++ backend/src/backend/API/Program.cs
@@ -1,3 +1,6 @@
+﻿using Application.Mapping;
+using Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// AutoMapper: quét tất cả Profile trong Application Layer
+builder.Services.AddAutoMapper(typeof(AppMappingProfile).Assembly);
+
+// Đăng ký các service khác
+builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 
