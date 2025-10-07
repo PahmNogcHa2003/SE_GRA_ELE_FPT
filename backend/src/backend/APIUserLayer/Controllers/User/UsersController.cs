40f19b438cc4b716ca8a7089a9a846d52606816b
TrungHa-IT <hatrung03022003@gmail.com>
2025-10-08 00:18:40 +0700

    update base for each role in system

---
+++ backend/src/backend/APIUserLayer/Controllers/User/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Application.DTOs;
+using Application.Interfaces.User.Service;
+using Application.Services.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIUserLayer.Controllers.User
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+      
+            private readonly IUserService _userService;
+
+            public UsersController(IUserService userService)
+            {
+                _userService = userService;
+            }
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsersByStatus()
+        {
+            var users = await _userService.GetPagedAsync(1,20);
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(long id)
+        {
+            var dto = await _userService.GetAsync(id);
+            if (dto == null) return NotFound();
+            return Ok(dto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserDTO dto)
+        {
+            var created = await _userService.CreateAsync(dto);
+            return Ok();
+        }
+    }
+}
---
+++ backend/src/backend/Application/Services/User/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Application.Interfaces.Base;
+using Application.Interfaces.User.Service;
+using Application.Services.Base;
+using AutoMapper;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.User
+{
+    public class UserService : GenericService<AspNetUser, UserDTO, long>, IUserService
+    {
+        public UserService(IRepository<AspNetUser, long> repo, IMapper mapper, IUnitOfWork uow)
+        : base(repo, mapper, uow) { }
+
+        public async Task<List<UserDTO>> GetAllStatusAsync(CancellationToken ct = default)
+        {
+            var entities = await _repo.Query()
+                .AsNoTracking()
+                .ToListAsync(ct);  // <--- thêm dòng này để thực thi truy vấn
+
+            return _mapper.Map<List<UserDTO>>(entities);
+        }
+
+    }
+}
