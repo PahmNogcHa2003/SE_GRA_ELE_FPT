48692428f0aa76f7c369c01fb0a2c7410e2e4999
TrungHa-IT <hatrung03022003@gmail.com>
2025-10-10 16:54:19 +0700

    create feature register and login with identity and jwt

---
+++ backend/src/backend/Application/Interfaces/Identity/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDTO> RegisterAsync(RegisterDTO model);
+        Task<AuthResponseDTO> LoginAsync(LoginDTO model);
+    }
+}
