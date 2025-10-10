f2a88a9392bb5bef26fdeb05d0aaa5d0404c7f6b
Phạm Ngọc Hà <pnha03dev.it@gmail.com>
2025-10-10 21:21:51 +0700

    Add-Migration AddAdminProfileAndUserVerified

---
+++ backend/src/backend/Domain/Entities/AdminProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class AdminProfile
+    {
+        public long UserId { get; set; }               
+        public string? FullName { get; set; }          
+        public string? Position { get; set; }           
+        public string? AvatarUrl { get; set; }         
+        public DateTimeOffset CreatedAt { get; set; }   
+        public DateTimeOffset UpdatedAt { get; set; }   
+
+        public virtual AspNetUser User { get; set; } = null!;
+    }
+}
