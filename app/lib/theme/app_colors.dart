84b67bf35d528983ca75cac73cf88a5c69c25457
b1g_nguyx <bichnq88@gmail.com>
2025-10-15 23:53:48 +0700

    refactor login, bottom, app_color

--- app/lib/theme/app_colors.dart
+++ app/lib/theme/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF43A047); // Xanh lá tươi nhẹ (Fresh Green)
-  static const accent = Color(0xFFFFCA28); // Vàng pastel điểm nhấn
+  static const primary = Color(0xFF43A047);
+  static const accent = Color(0xFFFFCA28);
   static const background = Color(0xFFF5FFF5);
   static const card = Colors.white;
   static const textPrimary = Color(0xFF1E1E1E);
